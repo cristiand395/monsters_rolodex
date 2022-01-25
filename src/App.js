174f762a1786293b4cc2e53697b83b27b2eb9965
cristian davila <cristiand395@gmail.com>
2022-01-26 00:36:02 +0100

    1.0v

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 import React from 'react'
 import './App.css';
 import { CardList } from './components/card-list/card-list.component'
+import { SearchBox } from './components/search-box/search-box.component'
 
 class App extends React.Component {
   constructor() {
     super();
 
     this.state = {
-      monsters : [
-        {
-          id : 'fran', 
-          name : 'Frankenstein'
-        },
-        {
-          id : 'dra',
-          name : 'Dracula'
-        },
-        {
-          id: 'zom',
-          name : 'Zombie'
-        }
-      ]
+      monsters: [],
+      searchField: ''
     }
   }
 
@@ -29,10 +18,22 @@ class App extends React.Component {
       .then(response => response.json())
       .then(users => this.setState({ monsters: users }))
   }
+
+  handleChange = e => {
+    this.setState({ searchField: e.target.value})
+  }
+
   render () {
+    const { monsters, searchField } = this.state
+    const filteredMonsters = monsters.filter(monster => 
+          monster.name.toLowerCase().includes(searchField.toLowerCase()))
     return (
       <div className="App">
-        <CardList monsters={this.state.monsters}/>
+        <h1>Robot Monster Dex</h1>
+        <SearchBox 
+          placeholder='search monster'
+          handleChange = {this.handleChange} />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
